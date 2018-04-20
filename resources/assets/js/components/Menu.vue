a6c19215bf47c2e6be00cd5711cdfb6dd442984f
rogue707dev <rogue901126@protonmail.com>
2018-04-21 00:37:06 +0200

    4 propositions de bouton login assez similaire, j'hesite entre la 2 (sobre) et la 3 couleur tranché

--- resources/assets/js/components/Menu.vue
+++ resources/assets/js/components/Menu.vue
@@ -1,5 +1,26 @@
 <template>
     <ul class="layout-conteneur__menu">
+<!--
+        <li class="layout-menu__item-log">
+            <svg width="30px" height="30px">
+                <use xlink:href="#login2"></use>
+            </svg>
+            Connexion
+        </li>
+
+        <rp-menu-item name="home" route="home" icon="login2" class="layout-menu__item layout-menu__item-log2">
+            Connexion
+        </rp-menu-item>
+-->
+        <rp-menu-item name="home" route="home" icon="login2" class="layout-menu__item layout-menu__item-log3">
+            Connexion
+        </rp-menu-item>
+         <!--
+        <rp-menu-item name="home" route="home" icon="login2" class="layout-menu__item layout-menu__item-log4">
+            Connexion
+        </rp-menu-item>
+-->
+
         <rp-menu-item name="home" route="home" icon="maison" class="layout-menu__item">
             Accueil
         </rp-menu-item>
