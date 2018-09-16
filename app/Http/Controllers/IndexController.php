a2c63b7ebcd63d3e43afc2e40d4ae8722652a0e0
rogue707dev <rogue901126@protonmail.com>
2018-09-16 22:25:14 +0200

    Modification du fichier html généré pour le front pour éviter le cache
    Il y a un cache de 24h sur les fichiers HTML sur le serveur. Cela permet
    d'éviter ce cache (car le fichier était un index) et empêche l'accès
    direct au fichier `front.html`.

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -6,6 +6,6 @@ class IndexController extends Controller
 {
     public function __invoke()
     {
-        return file_get_contents(__DIR__ . '/../../../public/index.html');
+        return file_get_contents(__DIR__ . '/../../../storage/front.html');
     }
 }
