2b53be40cb09258e337dfa45d03ebb7aece2ea0f
rogue707dev <rogue901126@protonmail.com>
2018-09-21 00:24:02 +0200

    Texte d'intro de l'alpha et de ce qu'est Radiophonix
    see #59

--- resources/assets/js/pages/Help.vue
+++ resources/assets/js/pages/Help.vue
@@ -7,7 +7,20 @@
         Page aide
 
 
+        <h2 class="h1">Qu'est-ce qu'un faiseur ?</h2>
+        <blockquote class="blockquote mb-5">
+            <p class="mb-0X">
+                blabla
+            </p>
+            <footer class="blockquote-footer">
+                <a href="https://wiki.netophonix.com/Saga_MP3">
+                    Extrait du Netowiki
+                </a>
+            </footer>
+        </blockquote>
 
+
+        @todo FAQ ?
 
 
 
--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -27,6 +27,52 @@
 
             <div class="row">
                 <div class="col-12">
+                    <h2 class="h1">Qu'est-ce que Radiophonix ?</h2>
+                    <blockquote class="blockquote mb-5">
+                        <p class="mb-0X">
+                            Radiophonix est une plateforme libre et gratuite de partage et
+                            d'écoute de sagas MP3.<br/>
+                            L'ambition de ce projet est de redonner vie à ce super média qu'est
+                            la saga MP3 en créant une plateforme qui facilite la publication et
+                            l'écoute des sagas.<br/>
+                            <br/>
+                            Ce projet est inspiré par les diverses tentatives de site similaire qui
+                            n'ont hélas jamais vu le jour, notamment l'<a title="Lien wiki Agoraphonix" href="https://wiki.netophonix.com/Agoraphonix">Agoraphonix</a>.<br/>
+                            Le nom du projet est bien évidemment inspiré de
+                            <a title="Lien Netophonix" href="https://netophonix.com/">Netophonix</a>.
+                        </p>
+                    </blockquote>
+
+                    <h2 class="h1">Version Alpha</h2>
+                    <blockquote class="blockquote mb-5">
+                        <p class="mb-0X">
+                            Le site est actuellement en version Alpha, où seule l'écoute de sagas
+                            est possible.<br/>
+                            Nous avons fait attention à ce que les sagas présentes actuellement sur
+                            le site ont toute une licence Creative Commons.<br/>
+                            La publication est en cours de développement.<br/>
+                            <br/>
+                            N'hésitez pas à laisser des remarques, signaler des bugs et
+                            faire des suggestions. On ne demande que ça !<br/>
+                            <br/>
+                            Radiophonix est un projet open source :
+                            <a title="Lien GitLab" href="https://gitlab.com/Radiophonix/Radiophonix">
+                                https://gitlab.com/Radiophonix/Radiophonix
+                            </a><br/>
+                            Les contributions sont les bienvenues !
+                        </p>
+                    </blockquote>
+
+                    <h2 class="h1">Qui êtes-vous ?</h2>
+                    <blockquote class="blockquote mb-5">
+                        <p class="mb-0X">
+                            Nous sommes deux développeurs web professionnels passionnés
+                            de sagas MP3.<br/>
+                            Faire des sites est notre métier depuis plusieurs années, ce qui
+                            nous permet de développer ce projet.
+                        </p>
+                    </blockquote>
+
                     <h2 class="h1">Qu'est-ce qu'une Saga MP3 ?</h2>
                     <blockquote class="blockquote mb-5">
                         <p class="mb-0X">
@@ -45,19 +91,6 @@
                             </a>
                         </footer>
                     </blockquote>
-
-                    <h2 class="h1">Qu'est-ce qu'un faiseur ?</h2>
-                    <blockquote class="blockquote mb-5">
-                        <p class="mb-0X">
-                            blabla
-                        </p>
-                        <footer class="blockquote-footer">
-                            <a href="https://wiki.netophonix.com/Saga_MP3">
-                                Extrait du Netowiki
-                            </a>
-                        </footer>
-                    </blockquote>
-
                 </div>
             </div>
 
@@ -116,7 +149,7 @@
 
         <div class="layout-conteneur__main">
 
-            Capture a mettre
+            @todo décrire ce qui est prévu
 
         </div>
 
@@ -130,13 +163,12 @@
         <headband
                 urlImage="/static/home/contribuer.jpeg"
                 title="Contribuer"
-                subtitle="Le site est open source sur GitLab et les contributions sont les bienvenues">
+                subtitle="Le site est open source et les contributions sont les bienvenues">
         </headband>
 
         <div class="layout-conteneur__main">
             <div class="row mb-5">
                 <div class="col-12">
-                    <h2 class="h1">Avancement</h2>
                     <p>Radiophonix est actuellement en <span class="badge badge-danger">Alpha</span></p>
                 </div>
             </div>
@@ -146,7 +178,7 @@
                     <p>
                         Les contributions sont les bienvenues !<br />
                         <br>
-                        Vous pouvez participer à la création du site sur GitLab.<br/>
+                        Vous pouvez participer à la création du site sur <a title="Lien GitLab" href="https://gitlab.com/Radiophonix/Radiophonix">GitLab</a>.<br/>
                         <br>
                         Vous pouvez aussi signaler les bugs rencontrés.
                     </p>
