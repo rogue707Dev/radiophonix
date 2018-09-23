bb6ec64571936bff9855b0cf9b57d0e648fa1e41
rogue707dev <rogue901126@protonmail.com>
2018-09-23 23:35:27 +0200

    reorga pour que ca soit mieux medium et small

--- resources/assets/js/components/publish/PublishPresentation.vue
+++ resources/assets/js/components/publish/PublishPresentation.vue
@@ -9,29 +9,40 @@
         <div class="layout-conteneur__main">
 
             <div class="row">
-                <div class="col-6">
-                    <div>
+                <div class="col-12 col-md-7">
+                    <p>
                         Radiophonix ne va pas se limiter à l'écoute de sagas MP3 ! Le site servira
-                        également à la publication.<br/>
+                        également à la publication.
+                    </p>
+                    <br/>
+                    <p>
                         Les créateurs auront accès à une interface permettant de publier des sagas,
                         avec toutes les infos liées.<br/>
                         <br/>
-                        Plein de choses sont prévues, par exemple :<br/>
-                        <ul>
-                            <li>Programmer à l'avance la publication des épisodes</li>
-                            <li>Avoir un flux RSS de podcast généré automatiquement par saga</li>
-                            <li>Afficher les pourcentages d'avancement du prochain épisode</li>
-                            <li>Renseigner les informations de la saga (liens, licence, synopsis, etc)</li>
-                            <li>Et beaucoup d'autres choses...</li>
-                        </ul>
-                        Cette partie "publication" est encore en cours de développement, et comme
-                        le reste du site, de façon ouverte sur
-                        <a href="https://gitlab.com/Radiophonix/Radiophonix" class="lien-paragraphe">GitLab</a>.<br/>
-                        <br/>
-                        Si vous savez développer du web vous êtes bien sûr invité.e.s à venir
-                        contribuer au code !
+                        Plein de choses sont prévues, par exemple :
+                    </p>
+                    <ul>
+                        <li>Programmer à l'avance la publication des épisodes</li>
+                        <li>Avoir un flux RSS de podcast généré automatiquement par saga</li>
+                        <li>Afficher les pourcentages d'avancement du prochain épisode</li>
+                        <li>Renseigner les informations de la saga (liens, licence, synopsis, etc)</li>
+                        <li>Et beaucoup d'autres choses...</li>
+                    </ul>
+                </div>
+                <div class="col-12 col-md-5">
+                    <div class="card mt-3">
+                        <img class="card-img-top"
+                             src="/static/home/publier-mock-infos-saga.png"
+                             alt="Informations d'une saga">
+                        <div class="card-footer text-muted">
+                            Informations d'une saga
+                        </div>
                     </div>
-                    <div class="card mt-4">
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-12 col-md-4">
+                    <div class="card">
                         <img class="card-img-top"
                              src="/static/home/publier-mock-calendrier.png"
                              alt="Programmation en avance de la publication">
@@ -40,21 +51,23 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-6">
+                <div class="col-12 col-md-3">
+                    <p class="my-sm-4">
+                        Cette partie "publication" est encore en cours de développement, et comme
+                        le reste du site, de façon ouverte sur
+                        <a href="https://gitlab.com/Radiophonix/Radiophonix" class="lien-paragraphe">GitLab</a>.<br/>
+                        <br/>
+                        Si vous savez développer du web vous êtes bien sûr invité.e.s à venir
+                        contribuer au code !
+                    </p>
+                </div>
+                <div class="col-12 col-md-5">
                     <div class="card">
                         <img class="card-img-top"
                              src="/static/home/publier-mock-collections.png"
                              alt="Liste des collections (saisons, bonus, making-of, etc.)">
                         <div class="card-footer text-muted">
                             Liste des collections (saisons, bonus, making-of, etc.)
-                        </div>
-                    </div>
-                    <div class="card mt-3">
-                        <img class="card-img-top"
-                             src="/static/home/publier-mock-infos-saga.png"
-                             alt="Informations d'une saga">
-                        <div class="card-footer text-muted">
-                            Informations d'une saga
                         </div>
                     </div>
                 </div>
