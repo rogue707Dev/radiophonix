e9c7aae1b9a9f40d948099be119c508075df89b5
rogue707dev <rogue901126@protonmail.com>
2018-09-29 20:22:34 +0200

    Refactor page 404

--- resources/assets/js/pages/errors/404.vue
+++ resources/assets/js/pages/errors/404.vue
@@ -1,11 +1,52 @@
 <template>
-    <div>
-        404
-    </div>
+
+    <root-layout>
+        <headband
+            urlImage="/static/home/faq.jpg"
+            title="Erreur 404"
+            subtitle="Page introuvable">
+        </headband>
+
+        <div class="container">
+
+            <h1 class="h1 mb-4">Sagas populaires</h1>
+
+            <saga-list :sagas="popular"></saga-list>
+
+        </div>
+    </root-layout>
+
 </template>
 
 <script>
-export default {
+    import api from '~/lib/api';
+    import RootLayout from '~/components/Ui/Layout/RootLayout';
+    import Headband from '~/components/content/Headband';
+    import SagaList from '~/components/saga/SagaList';
 
-}
+    export default {
+        components: {
+            RootLayout,
+            Headband,
+            SagaList,
+        },
+
+        data() {
+            return {
+                popular: [],
+            }
+        },
+
+        methods: {
+            async fetchPopular() {
+                let result = await api.sagas.popular();
+
+                this.popular = result.data;
+            }
+        },
+
+        mounted() {
+            this.fetchPopular();
+        },
+    }
 </script>
