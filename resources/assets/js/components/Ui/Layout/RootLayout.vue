4cb32996baf34c7807b0897c84690ac8a6ebff0b
rogue707dev <rogue901126@protonmail.com>
2018-09-29 20:17:42 +0200

    Layout déplacé dans un composant pour être réutilisable

---
+++ resources/assets/js/components/Ui/Layout/RootLayout.vue
@@ -0,0 +1,53 @@
+<template>
+
+    <div class="layout-global">
+
+        <div class="layout-global__header">
+            <header-mobile></header-mobile>
+        </div>
+
+        <div class="layout-global__menu" :class="{actif: isMenuOpen, inactif: !isMenuOpen}">
+            <main-menu></main-menu>
+        </div>
+
+        <div class="layout-global__lecteur" :class="playerClass">
+            <player-normal></player-normal>
+        </div>
+
+        <div class="layout-global__playlist" :class="playlistClass">
+            <player-playlist></player-playlist>
+        </div>
+
+        <div class="layout-global__main" :class="mainClass">
+            <slot></slot>
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+    import { mapState } from 'vuex';
+    import MainMenu from '~/components/Ui/Menu/MainMenu';
+    import HeaderMobile from '~/components/menu/HeaderMobile.vue';
+    import PlayerNormal from '~/components/player/PlayerNormal.vue';
+    import PlayerPlaylist from '~/components/player/PlayerPlaylist.vue';
+
+    export default {
+        components: {
+            MainMenu,
+            HeaderMobile,
+            PlayerNormal,
+            PlayerPlaylist,
+        },
+
+        computed: {
+            ...mapState('ui', [
+                'isMenuOpen',
+                'mainClass',
+                'playerClass',
+                'playlistClass',
+            ])
+        },
+    }
+</script>
--- resources/assets/js/pages/Layout.vue
+++ resources/assets/js/pages/Layout.vue
@@ -1,53 +1,17 @@
 <template>
 
-    <div class="layout-global">
-
-        <div class="layout-global__header">
-            <header-mobile></header-mobile>
-        </div>
-
-        <div class="layout-global__menu" :class="{actif: isMenuOpen, inactif: !isMenuOpen}">
-            <main-menu></main-menu>
-        </div>
-
-        <div class="layout-global__lecteur" :class="playerClass">
-            <player-normal></player-normal>
-        </div>
-
-        <div class="layout-global__playlist" :class="playlistClass">
-            <player-playlist></player-playlist>
-        </div>
-
-        <div class="layout-global__main" :class="mainClass">
-            <router-view></router-view>
-        </div>
-
-    </div>
+    <root-layout>
+        <router-view></router-view>
+    </root-layout>
 
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import MainMenu from '~/components/Ui/Menu/MainMenu';
-import HeaderMobile from '~/components/menu/HeaderMobile.vue';
-import PlayerNormal from '~/components/player/PlayerNormal.vue';
-import PlayerPlaylist from '~/components/player/PlayerPlaylist.vue';
+import RootLayout from '~/components/Ui/Layout/RootLayout';
 
 export default {
     components: {
-        MainMenu,
-        HeaderMobile,
-        'player-normal': PlayerNormal,
-        'player-playlist': PlayerPlaylist,
-    },
-
-    computed: {
-        ...mapState('ui', [
-            'isMenuOpen',
-            'mainClass',
-            'playerClass',
-            'playlistClass',
-        ])
+        RootLayout,
     },
 }
 </script>
