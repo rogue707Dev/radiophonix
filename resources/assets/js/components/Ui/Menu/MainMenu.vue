f20659a081e65244a54f353695a4b211c19b8c15
rogue707dev <rogue901126@protonmail.com>
2018-09-30 19:17:08 +0200

    Composant `MenuItem` renommé en `MainMenuItem`
    Le tag `<menu-item>` est réservé en HTML.

--- resources/assets/js/components/Ui/Menu/MainMenu.vue
+++ resources/assets/js/components/Ui/Menu/MainMenu.vue
@@ -1,39 +1,57 @@
 <template>
     <ul class="layout-conteneur__menu">
-        <menu-item name="login"
+<!--
+        <li class="layout-menu__item-log">
+            <svg width="30px" height="30px">
+                <use xlink:href="#login2"></use>
+            </svg>
+            Connexion
+        </li>
+
+        <main-menu-item name="home" route="home" icon="login2" class="layout-menu__item layout-menu__item-log2">
+            Connexion
+        </main-menu-item>
+-->
+        <main-menu-item name="login"
                         route="login"
                         icon="login2"
                         class="layout-menu__item layout-menu__item-log3"
                         v-feature="'login'">
             Connexion
-        </menu-item>
-        <menu-item name="home" route="home" icon="maison" class="layout-menu__item">
+        </main-menu-item>
+         <!--
+        <main-menu-item name="home" route="home" icon="login2" class="layout-menu__item layout-menu__item-log4">
+            Connexion
+        </main-menu-item>
+-->
+
+        <main-menu-item name="home" route="home" icon="maison" class="layout-menu__item">
             Accueil
-        </menu-item>
-        <menu-item name="search" route="search" icon="rechercher" class="layout-menu__item">
+        </main-menu-item>
+        <main-menu-item name="search" route="search" icon="rechercher" class="layout-menu__item">
             Rechercher
-        </menu-item>
-        <menu-item name="listen" route="listen.home" icon="play" class="layout-menu__item">
+        </main-menu-item>
+        <main-menu-item name="listen" route="listen.home" icon="play" class="layout-menu__item">
             Écouter
-        </menu-item>
-        <menu-item name="publish" route="publish" icon="edit" class="layout-menu__item">
+        </main-menu-item>
+        <main-menu-item name="publish" route="publish" icon="edit" class="layout-menu__item">
             Publier
-        </menu-item>
-        <menu-item name="contribute" route="contribute" icon="contribuer" class="layout-menu__item">
+        </main-menu-item>
+        <main-menu-item name="contribute" route="contribute" icon="contribuer" class="layout-menu__item">
             Contribuer
-        </menu-item>
-        <menu-item name="help" route="help" icon="info" class="layout-menu__item">
+        </main-menu-item>
+        <main-menu-item name="help" route="help" icon="info" class="layout-menu__item">
             Aide
-        </menu-item>
+        </main-menu-item>
     </ul>
 </template>
 
 <script>
-    import MenuItem from '~/components/Ui/Menu/MenuItem';
+    import MainMenuItem from '~/components/Ui/Menu/MainMenuItem';
 
     export default {
         components: {
-            MenuItem,
+            MainMenuItem,
         }
     }
 </script>
