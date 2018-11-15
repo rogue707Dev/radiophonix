9edfde78c187b263084716c575a1a74369f0674d
rogue707dev <rogue901126@protonmail.com>
2018-11-15 12:43:20 +0100

    Ajout d'un lien vers la home sur le logo du header mobile

--- resources/assets/js/components/menu/HeaderMobile.vue
+++ resources/assets/js/components/menu/HeaderMobile.vue
@@ -16,10 +16,12 @@
             <span class="h6">Menu</span>
         </button>
 
-        <svg width="150px" height="25px" class="fill--blanc mb-1">
-            <use class="fill--logo-orange" xlink:href="#logo-part1of2"></use>
-            <use xlink:href="#logo-part2of2"></use>
-        </svg>
+        <router-link :to="{ name: 'home' }">
+            <svg width="150px" height="25px" class="fill--blanc mb-1">
+                <use class="fill--logo-orange" xlink:href="#logo-part1of2"></use>
+                <use xlink:href="#logo-part2of2"></use>
+            </svg>
+        </router-link>
 
     </div>
 </template>
