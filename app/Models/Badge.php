897d01ce3946b1dc7cf20263b0233654d1cf78e1
rogue707dev <rogue901126@protonmail.com>
2019-04-23 23:25:25 +0200

    Ajout de la notion de badges en BDD
    see #117

---
+++ app/Models/Badge.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Radiophonix\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property string $key
+ * @property string $title
+ * @property string $description
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * @property-read Collection|User[] $users
+ */
+class Badge extends Model
+{
+    public function awardTo(User $user): void
+    {
+        $this->users()->attach($user);
+
+        $this->save();
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'user_badges');
+    }
+}
---
+++ database/migrations/2019_04_23_194735_create_badges_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBadgesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('badges', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('key')->unique();
+            $table->string('title');
+            $table->string('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('badges');
+    }
+}
