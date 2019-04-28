05f30b0a7be57904aef3d6559a1c56e8a565300c
rogue707dev <rogue901126@protonmail.com>
2019-04-29 00:11:04 +0200

    Suppression du nom de l'application dans la conf des backups
    En supprimant le nom, le système de backups arrive correctement à lister
    les fichiers présent et donc à analyser les fichiers, ce qui corriger le
    monitoring.
    
    closes #127

--- config/backup.php
+++ config/backup.php
@@ -10,7 +10,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name' => env('APP_NAME', 'Radiophonix'),
+        'name' => '',
 
         'source' => [
 
@@ -164,10 +164,9 @@ return [
      */
     'monitor_backups' => [
         [
-            'name' => env('APP_NAME', 'Radiophonix'),
+            'name' => '',
             'disks' => [
-                // @todo issue #127
-//                'backups-dropbox-1',
+                'backups-dropbox-1',
                 'backups-dropbox-2',
             ],
             'health_checks' => [
