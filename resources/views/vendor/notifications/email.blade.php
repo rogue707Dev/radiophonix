acc32707d0979e6d60e8fb177adf4d1af9fae53a
rogue707dev <rogue901126@protonmail.com>
2019-05-05 23:03:46 +0200

    Surcharge du template des emails

---
+++ resources/views/vendor/notifications/email.blade.php
@@ -0,0 +1,55 @@
+@component('mail::message')
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops !')
+@else
+# @lang('Bonjour !')
+@endif
+@endif
+
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    switch ($level) {
+        case 'success':
+        case 'error':
+            $color = $level;
+            break;
+        default:
+            $color = 'primary';
+    }
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
+
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
+
+@endforeach
+
+{{-- Subcopy --}}
+@isset($actionText)
+@slot('subcopy')
+@lang(
+    "Si le bouton \":actionText\" ne fonctionne pas, vous pouvez copier/coller cette URL\n".
+    'dans votre navigateur: [:actionURL](:actionURL)',
+    [
+        'actionText' => $actionText,
+        'actionURL' => $actionUrl,
+    ]
+)
+@endslot
+@endisset
+@endcomponent
