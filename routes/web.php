2ea66536eeb104402d4c8a070029def0085a5360
rogue707dev <rogue901126@protonmail.com>
2019-05-05 23:09:33 +0200

    Ajout d'une explication sur la route de reset du mot de passe

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,10 @@
 use Radiophonix\Http\Controllers\IndexController;
 
 Route::get('/', IndexController::class);
+
+/**
+ * Cette route est une copie de celle présente côté front.
+ * Elle est nécessaire pour pouvoir générer un lien lors de l'envoi
+ * de l'email de réinitialisation de mot de passe.
+ */
 Route::get('/password-reset/{token}', IndexController::class)->name('password-reset');
