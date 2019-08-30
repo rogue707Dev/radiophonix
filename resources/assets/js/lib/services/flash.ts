48d06a74b547768b4fc030c83b067d1c7ee05194
rogue707dev <rogue901126@protonmail.com>
2019-08-30 14:12:55 +0200

    Conversion en TypeScript

--- resources/assets/js/lib/services/flash.ts
+++ resources/assets/js/lib/services/flash.ts
@@ -1,4 +1,4 @@
-import Swal from 'sweetalert2';
+import Swal, {SweetAlertOptions} from 'sweetalert2';
 
 function sendFlashMessage(type, message, title = null) {
     return Swal.fire({
@@ -8,22 +8,27 @@ function sendFlashMessage(type, message, title = null) {
     });
 }
 
-const flash = window.flash = {
+const flash = {
     info: (message, title) => sendFlashMessage('info', message, title),
     success: (message, title) => sendFlashMessage('success', message, title),
     warning: (message, title) => sendFlashMessage('warning', message, title),
     error: (message, title) => sendFlashMessage('error', message, title),
     confirm: (message, title, withPasswordInput = false) => {
-        return Swal.fire({
+        let config: SweetAlertOptions = {
             title: title,
             text: message,
             type: 'warning',
-            input: withPasswordInput ? 'password' : '',
             showConfirmButton: true,
             confirmButtonText: 'Oui',
             showCancelButton: true,
             cancelButtonText: 'Non',
-        });
+        };
+
+        if (withPasswordInput) {
+            config.input = 'password';
+        }
+
+        return Swal.fire(config);
     },
     close: () => {
         Swal.close();
